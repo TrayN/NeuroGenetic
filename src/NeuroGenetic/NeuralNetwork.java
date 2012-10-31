675438daa3c903672d4f36d2db6c289ba7c4e763
TrayNo <TrayNo@192.168.1.140>
2012-10-31 20:33:12 +0200

    Создание макета нейронной сети

---
+++ src/NeuroGenetic/NeuralNetwork.java
@@ -0,0 +1,33 @@
+package NeuroGenetic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author TrayNo
+ */
+public class NeuralNetwork {
+    
+    private ArrayList <Neuron> incomeLayout = new ArrayList <>();
+    private ArrayList <Neuron> outcomeLayout = new ArrayList <>();
+    private ArrayList <ArrayList <Neuron> > hideLayouts = new ArrayList < >();
+    private int incomeLayoutNeuronCount, outcomeLayoutNeuronCount, hideLayoutsCount;
+    
+    public NeuralNetwork(int incomeLayoutNeuronCount, int outcomeLayoutNeuronCount, int hideLayoutsCount){
+        
+    }
+    
+    public void setIncomeParams(){
+    
+    }
+    
+    public void setOutcomeValue(){
+    
+    }
+    
+    public double getOutcomeValue(){
+        return 0;
+    }
+    
+    
+}
--- src/NeuroGenetic/Neuron.java
+++ src/NeuroGenetic/Neuron.java
@@ -1,9 +1,35 @@
 package NeuroGenetic;
 
+import java.util.HashMap;
+
 /**
  *
  * @author TrayNo
  */
 public class Neuron {
     
+    private HashMap <Integer, Double> incomeSynapsesKoef;
+    private HashMap <Integer, Double> incomeSynapsesValues = new HashMap <Integer, Double>();
+    private double outcomeValue = 0;
+    private int incomeSynapsesCount;
+    
+    public Neuron(int incomeSynapsesCount, HashMap <Integer, Double> initializingKoefArr){
+        this.incomeSynapsesKoef = initializingKoefArr;
+        this.incomeSynapsesCount = incomeSynapsesCount;
+    }
+    
+    public void setValue(int synapseNum, double value){
+        incomeSynapsesValues.put(synapseNum, value);
+    }
+    
+    public double getOutcomeValue(){
+        return outcomeValue;
+    }
+    
+    public void countOutcomeValue(){
+        outcomeValue = 0;
+        for(int i=0; i < incomeSynapsesCount; i++){
+            outcomeValue+=incomeSynapsesValues.get(i)*incomeSynapsesKoef.get(i);
+        }
+    }
 }
